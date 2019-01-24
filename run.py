3e5457b9a052c0fc9b3294254629646b0fbdaaba
Brook Kynaston <brookkynaston@live.fr>
2019-01-24 01:53:15 +0000

    Highscore board functionality complete
    highscore board now works with session variables to take the user's name and score, adds to a list and sorts it high to low. The list will also not exceed 11 entries. Need to tidy code in run.py and style the game_over and highscores templates

--- run.py
+++ run.py
@@ -13,51 +13,33 @@ MAX_ATTEMPTS = 2
 with open("data/riddles.json") as riddle_file:
     RIDDLES = json.load(riddle_file)
 
-def write_to_leaderboard(player, score):
-    now = datetime.now().strftime("%H:%M:%S")
-    
-    with open('data/highscore_board.txt', 'a') as leaderboard:
-        leaderboard.write(str(player) + "\n" + str(score) + "\n")
-    leaderboard.close()
-        
-    
-
-def check_length_and_order():
-    players = []
-    scores = []
-    now = datetime.now().strftime("%H:%M:%S")
-    
-    with open('data/highscore_board.txt', 'r') as leaderboard:
-        lines = leaderboard.read().splitlines()
-    
-    for i, text in enumerate(lines):
-        if i%2 == 0:
-            players.append(text)
-        else:
-            scores.append(text)
-    
-    players_scores = zip(players, scores)
-    
-    sorted_highscores = sorted(players_scores, key=lambda tup: tup[1])
-    """
-    return sorted_highscores  
-     
-     Currently returning the sorted list of tuples (players_scores), next will need to figure out how to delete the entries > 11 and print to highscore board"""
-    
-
-    
-
-    
-
-
-
-
+usernames = []
+scores = []
 wrong_answers = []
 
 high_score = {
     "name": "nobody",
     "score": 0
 }
+
+def add_to_scoreboard(name, score):
+    """takes users names and scores and appends them each to a list, to be used in the highscore board"""    
+    usernames.append(name)
+    scores.append(score)
+    
+def check_length_and_order():
+    """takes the usernames and scores lists, combines them and sorts each user from high to low on their score"""
+    
+    usernames_and_scores = sorted(zip(usernames, scores), key=lambda x: x[1], reverse=True)
+    
+    """keeps the length of the list to 11 entries"""
+    if len(usernames_and_scores) >= 12:
+        
+        top_11 = usernames_and_scores[:11]
+        return top_11
+    
+    return usernames_and_scores
+    
 
 @app.route("/")
 def index():
@@ -72,8 +54,6 @@ def new_game():
     session["riddle_attempts"] = MAX_ATTEMPTS
     session["wrong_answers"] = wrong_answers
      
-    
- 
     return redirect(url_for("riddle"))
 
 @app.route("/about", methods=["GET","POST"])
@@ -82,31 +62,31 @@ def about():
 
 @app.route("/highscores", methods=["GET","POST"])
 def highscores():
-    return render_template("highscores.html")
+    
+    session["highscore_board"] = check_length_and_order()
+    
+    return render_template("highscores.html", highscore_board = session["highscore_board"])
 
 @app.route("/riddle", methods=["GET", "POST"])
 def riddle():
     
-    
+    """If the username has not been logged at the homepage, the user will be redirected there"""
     
     if "player" not in session:
         return redirect(url_for("index"))
-
+    
+    """When the user submits an answer the answer is checked from the ridddles.json file. If it is correct, their score and riddle_num is incremented and a feedback message is displayed. If not their riddle_attempts is incremented and a message is displayed to show that the answer was wrong. After 3 wrong attmpts, the current riddle is skipped"""
+    
+    
     if request.method == "POST" and session["riddle_num"] < len(RIDDLES): 
         
         previous_riddle = RIDDLES[session["riddle_num"]]
         
         answer = request.form["answer"].lower()
         
-       
-        
-        
-        
-        
         if answer == previous_riddle["answer"]:
             session["riddle_num"] += 1
             session["score"] += 1
-            
             
             if session["riddle_num"] < len(RIDDLES): 
                 flash("Correct answer, %s! Your score is %s." % (
@@ -114,62 +94,36 @@ def riddle():
                 session["wrong_answers"]=[]
                 session["riddle_attempts"] = MAX_ATTEMPTS
                 
-        
         elif not session["riddle_attempts"] and session["wrong_answers"]:
             session["riddle_num"] += 1
             session["riddle_attempts"] = MAX_ATTEMPTS
             session["wrong_answers"] = wrong_answers
             
-            
             if session["riddle_num"] < len(RIDDLES):
                 flash("Wrong answer, %s. Better luck with this riddle:" % (
                       session["player"]))
-                
-                
-                
-                
+        
         else:
             
             session["riddle_attempts"] -= 1
-            
             session["wrong_answers"].append(answer)
-            
             flash("Wrong answer, %s. You have %s attempts left." % (
             session["player"], session["riddle_attempts"]+1))
-             
-            
-            
-            
-
-    if session["riddle_num"] >= len(RIDDLES):
-        """
-        write_to_leaderboard(session["player"],session["score"])
-        check_length_and_order()
-        """
-        
-        
-        
-        
-        if session["score"] >= high_score["score"]:
-            high_score["score"] = session["score"]
-            high_score["name"] = session["score"]
-        
     
-    flash("Your final score is %s, %s /n The current highscore is held by %s, with %s points" % (session["score"], session["score"], high_score["name"],high_score["score"]))
+    """ When all the riddles are asked, the players name and score is added to the highscore board"""        
+
+    if session["riddle_num"] >= len(RIDDLES): 
+    
+        add_to_scoreboard(session["player"], session["score"])
         
-    """ add an if statement to riddle.html that only shows gollum/hides the ridddle and question input box, so that user cannot input anything else and break the code"""   
-        
-    """
-        return render_template("highscores.html", player=session["player"],
-                               score=session["score"],
-                               highscore=high_score["score"],
-                               highscorer=high_score["name"])
-    """
+        return render_template("game_over.html", player=session["player"],
+                               score=session["score"])
+    
     new_riddle = RIDDLES[session["riddle_num"]]
     
-    return render_template(
-        "riddle.html", player=session["player"],
-        question=new_riddle["question"], riddle_num=session["riddle_num"], wrong_answers=session["wrong_answers"], attempts=session["riddle_attempts"])
+    return render_template("riddle.html", player=session["player"],
+            question=new_riddle["question"], riddle_num=session["riddle_num"], 
+            wrong_answers=session["wrong_answers"], attempts=session["riddle_attempts"])
 
 
 if __name__ == "__main__":
